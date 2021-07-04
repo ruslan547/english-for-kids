c22f8dea07fcfd3a30a94fc021806bee679ef48e
ruslan547 <novik.rv93@gmail.com>
2021-07-04 19:34:27 +0300

    feat: add debug

--- english-for-kids-admin-panel/src/bin/efk.ts
+++ english-for-kids-admin-panel/src/bin/efk.ts
@@ -2,14 +2,19 @@
 
 require('dotenv').config();
 
+const debug = require('debug')('efk:server');
 const app = require('../app');
 
 const PORT = process.env.PORT || 4000;
+const APP_NAME = 'efk';
 
 const onListening = () => {
-  const str = `Listening on ${PORT}\n`;
+  const message = `Listening on ${PORT}`;
 
-  process.stdout.write(str);
+  process.stdout.write(message);
+  debug(message);
 };
+
+debug('booting %o', APP_NAME);
 
 app.listen(PORT, onListening);
