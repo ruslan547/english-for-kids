1f82059ffc31b41f734d34e6ce50968c988ecf82
ruslannovik <novik.rv93@gmail.com>
2021-07-17 12:52:47 +0300

    fix: delete auth middleware from category router

--- english-for-kids-admin-panel/server/src/routes/category.ts
+++ english-for-kids-admin-panel/server/src/routes/category.ts
@@ -3,12 +3,11 @@ import {
   createCategory, deleteCategory, getCategories, updateCategory,
 } from '../controllers/category';
 import pathsConstants from '../constants/paths';
-import auth from '../middleware/auth';
 
 const categoryRouter = Router();
 
 categoryRouter.get(pathsConstants.DEFAULT, getCategories);
-categoryRouter.post(pathsConstants.DEFAULT, auth, createCategory);
+categoryRouter.post(pathsConstants.DEFAULT, createCategory);
 categoryRouter.patch(pathsConstants.DEFAULT, updateCategory);
 categoryRouter.delete(pathsConstants.DEFAULT, deleteCategory);
 
